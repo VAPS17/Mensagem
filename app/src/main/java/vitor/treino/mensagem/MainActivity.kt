65f8364a5bf5bd1fd81cae327de67ea48451a503
Victor Seguro <seguro17@gmail.com>
2021-04-21 10:50:01 +0100

    Obter a mensagem introduzida pelo utilizador

--- app/src/main/java/vitor/treino/mensagem/MainActivity.kt
+++ app/src/main/java/vitor/treino/mensagem/MainActivity.kt
@@ -1,8 +1,10 @@
 package vitor.treino.mensagem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun EnviarMenagem(view: View) {}
+    fun EnviarMenagem(view: View) {
+        val editTextMensagem = findViewById<EditText>(R.id.editTextTextMessage)
+
+        val mensagem = editTextMensagem.text.toString()
+
+        val intent = Intent(this, MostraMensagemActivity::class.java)
+
+
+        startActivity(intent)
+    }
 }
